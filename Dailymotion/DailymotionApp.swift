efd754e0b4ba19b834be9ef961fb2d08c273c81d
Ramill <ramill_ibragimov@icloud.com>
2022-05-14 13:16:59 +0300

    Initial Commit

---
+++ Dailymotion/DailymotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailymotionApp.swift
+//  Dailymotion
+//
+//  Created by Ramill Ibragimov on 14.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct DailymotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
