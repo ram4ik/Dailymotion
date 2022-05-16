11bcdd8d9d8236f71b3d70007bdd10633a4bb070
Ramill <ramill_ibragimov@icloud.com>
2022-05-16 15:53:21 +0300

    an image for the channel

--- Dailymotion/Views/ChannelView.swift
+++ Dailymotion/Views/ChannelView.swift
@@ -13,14 +13,29 @@ struct ChannelView: View {
     var body: some View {
         NavigationView {
             List(service.channelData) { channel in
-                VStack(alignment: .leading) {
-                    Text(channel.name)
-                        .font(.title)
-                        .bold()
-                        .padding()
-                    Text(channel.listDescription)
-                        .foregroundColor(.secondary)
-                        .padding(.horizontal)
+                HStack {
+                    ZStack {
+                        Image("placeholder")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50, alignment: .center)
+                            .cornerRadius(.infinity)
+                        Image("\(channel.id)")
+                            .resizable()
+                            .frame(width: 50, height: 50, alignment: .center)
+                            .scaledToFit()
+                            .cornerRadius(.infinity)
+                    }.shadow(color: .secondary, radius: 4, x: 0, y: 3)
+                    
+                    VStack(alignment: .leading) {
+                        Text(channel.name)
+                            .font(.title)
+                            .bold()
+                            .padding()
+                        Text(channel.listDescription)
+                            .foregroundColor(.secondary)
+                            .padding(.horizontal)
+                    }
                 }
             }
             .listStyle(PlainListStyle())
