013f526f228a173d02a2083160815ff31f5dd518
Ramill <ramill_ibragimov@icloud.com>
2022-05-17 10:19:20 +0300

    Close AVPlayer when movie is complete

--- Dailymotion/Views/UIVideoPlayerController.swift
+++ Dailymotion/Views/UIVideoPlayerController.swift
@@ -18,6 +18,8 @@ class UIVideoPlayerController: UIViewController {
         "https://storage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
     ]
     
+    let vc = AVPlayerViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -29,9 +31,16 @@ class UIVideoPlayerController: UIViewController {
         
         let player = AVPlayer(url: url)
         
-        let vc = AVPlayerViewController()
+        
         vc.player = player
-        vc.player?.play()
-        present(vc, animated: true)
+        NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: vc.player?.currentItem)
+        
+        present(vc, animated: true) { [self] in
+            vc.player?.play()
+        }
+    }
+    
+    @objc func playerDidFinishPlaying(note: NSNotification) {
+        vc.dismiss(animated: true, completion: nil)
     }
 }
