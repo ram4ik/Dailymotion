f61a55fdbc0a2a1f8e6a666adc966349961ce59f
Ramill <ramill_ibragimov@icloud.com>
2022-05-18 15:02:11 +0300

    fix bug: remove unnecessary UIVideoPlayerController(extra level of controller) avoid force-unwrapping

--- Dailymotion/Views/VideoPlaybackControllerView.swift
+++ Dailymotion/Views/VideoPlaybackControllerView.swift
@@ -5,15 +5,27 @@
 //  Created by Ramill Ibragimov on 17.05.2022.
 //
 
+import AVKit
 import SwiftUI
 
 struct VideoPlaybackControllerView: UIViewControllerRepresentable {
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+    
+    func makeUIViewController(context: Context) -> AVPlayerViewController {
+       
+        guard let randomVideoURL = PreConfiguredVideos.randomVideos.randomElement(),
+              let url = URL(string: randomVideoURL) else {
+            
+            assertionFailure("Unexpected `randomVideo` url is nil.")
+            return AVPlayerViewController()
+        }
         
-    }
-
-    func makeUIViewController(context: Context) -> some UIVideoPlayerController {
-        return UIVideoPlayerController()
+        let player = AVPlayer(url: url)
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        return playerViewController
     }
     
+    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {
+        
+    }
 }
